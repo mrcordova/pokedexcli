d35a7dc154013c0f8fa0da6f7818536f2d008242
mrcordova <46950394+mrcordova@users.noreply.github.com>
2025-02-09 09:27:31 -0800

    create repl cli

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 func main()  {
-	fmt.Printf("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for  {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		// if !moreCmds {
+		// 	break
+		// }
+		cmd := scanner.Text()
+		toLowerCmd := strings.ToLower(cmd)
+		words := strings.Fields(toLowerCmd)
+		fmt.Println("Your command was:", words[0])
+
+	}
 }
 
