aacb06b19a30b901ddd91b6e4a51b1abf6e79752
mrcordova <46950394+mrcordova@users.noreply.github.com>
2025-02-09 09:29:35 -0800

    move repl to designated file

--- main.go
+++ main.go
@@ -1,24 +1,7 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-func main()  {
-	scanner := bufio.NewScanner(os.Stdin)
-	for  {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		// if !moreCmds {
-		// 	break
-		// }
-		cmd := scanner.Text()
-		toLowerCmd := strings.ToLower(cmd)
-		words := strings.Fields(toLowerCmd)
-		fmt.Println("Your command was:", words[0])
 
-	}
+func main()  {
+	startRepl()
 }
 
